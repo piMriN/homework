d5de038c4b025b614de2195d135c50cb2009652b
MaNa0923 <1491343463@qq.com>
2022-07-19 20:31:51 +0800

    '复选框、索引、宽度属性配置,自定义渲染文本,插槽渲染组件,作用域插槽传输数据'

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -2,12 +2,49 @@
   <div>
     <el-table :data="tableData" style="width: 100%">
       <el-table-column
-        v-for="item in column"
-        :key="item.prop"
-        :prop="item.prop"
-        :label="item.label"
-      >
-      </el-table-column>
+        v-if="index"
+        label="序号"
+        type="index"
+        width="55"
+      ></el-table-column>
+      <el-table-column
+        v-if="checkbox"
+        type="selection"
+        width="55"
+      ></el-table-column>
+      <template v-for="(item, index) in column">
+        <el-table-column
+          v-if="item.type === 'function'"
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+        >
+          <template v-slot="scope">
+            <div
+              v-html="item.callback && item.callback(scope.row, index)"
+            ></div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-if="item.type === 'slot'"
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+        >
+          <template v-slot="scope">
+            <slot :name="item.slot_name" :data="scope.row"></slot>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-else
+          :key="index"
+          :prop="item.prop"
+          :label="item.label"
+          :width="item.width"
+        ></el-table-column>
+      </template>
     </el-table>
   </div>
 </template>
@@ -17,36 +54,44 @@ export default {
   name: "Table",
   props: {
     column: {
-      typeof: Array,
+      type: Array,
       default: () => [],
     },
+    checkbox: Boolean,
+    index: Boolean,
   },
   data() {
     return {
       tableData: [
         {
-          date: "2016-05-02",
+          id: 1,
+          title: "这是一段标题1",
+          date: "2016-05-02", 
           name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄",
-          sex: "女",
+          address: "上海市<b>普陀</b><div>123456</div>区金沙江路 1518 弄",
+          sex: "男",
         },
         {
+          id: 2,
+          title: "这是一段标题2",
           date: "2016-05-04",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1517 弄",
           sex: "女",
         },
         {
+          title: "这是一段标题3",
           date: "2016-05-01",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1519 弄",
           sex: "女",
         },
         {
+          title: "这是一段标题4",
           date: "2016-05-03",
           name: "王小虎",
           address: "上海市普陀区金沙江路 1516 弄",
-          sex: "女", 
+          sex: "男",
         },
       ],
     };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
-    <na-table :column='column'></na-table>
+    <na-table :column="column" checkbox index>
+      <template v-slot:operation="slot">
+        <el-button type="primary" @click="handleEdit(slot.data)"
+          >编辑</el-button
+        >
+        <na-button type="danger" @click="handleDelete(slot.data)"
+          >删除</na-button
+        >
+      </template>
+    </na-table>
   </div>
 </template>
 
@@ -9,16 +18,35 @@ export default {
   name: "Home",
   data() {
     return {
-      column:[
-        {label:'日期',prop:'date'},
-        {label:'姓名',prop:'name'},
-        {label:'地址',prop:'address'},
-        {label:'性别',prop:'sex'}
-      ]
+      column: [
+        {
+          label: "标题",
+          prop: "title",
+          type: "function",
+          callback: (row) => {
+            if (row.id === 1) {
+              return `<a href="https://www.baidu.com">${row.title}</a>`;
+            }
+            return `<span>${row.title}</span>`;
+          },
+        },
+        // { label: "日期", prop: "date", Width: 500 },
+        { label: "姓名", prop: "name" },
+        { label: "地址", prop: "address" },
+        { label: "性别", prop: "sex" },
+        {
+          label: "操作",
+          prop: "operation",
+          type: "slot",
+          slot_name: "operation",
+          prop: "operation",
+        },
+      ],
     };
   },
   components: {
     naTable: () => import("../components/table/index.vue"),
+    naButton: () => import("../components/button/index.vue"),
   },
   methods: {},
 };
