fca17511168cffa65156802bc08cdfb764cf8a9f
MaNa0923 <1491343463@qq.com>
2022-07-26 21:12:05 +0800

    'all'

--- src/components/control/demoSelect/index.vue
+++ src/components/control/demoSelect/index.vue
@@ -1,108 +1,113 @@
 <template>
   <div>
-    <el-select v-model="val" @change="handleChangeEvent" >
-      <el-option  v-for="item in options" :key="item[props.value]" :label="item[props.label]" :value="item[props.value]"></el-option>
+    <el-select v-model="val" @change="handleChangeEvent">
+      <el-option
+        v-for="item in options"
+        :key="item[props.value]"
+        :label="item[props.label]"
+        :value="item[props.value]"
+      ></el-option>
     </el-select>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SelectComponent',
+  name: "SelectComponent",
   props: {
     value: {
       type: [String, Number],
-      default: ''
+      default: "",
     },
     config: {
       type: Object,
-      default: () => ({})
-    }
+      default: () => ({}),
+    },
   },
   watch: {
     value: {
-      handler (newValue) {
-        this.val = newValue
-      },
-      immediate: true
-    },
-    config: {
-      handler (val) {
-        this.initOptions()
-        this.initProps()
+      handler(newValue) {
+        this.val = newValue;
       },
       immediate: true,
-      deep: true
-    }
+    },
+    config: {
+      handler(val) {
+        this.initOptions();
+        this.initProps();
+      },
+      immediate: true,
+      deep: true,
+    },
   },
-  data () {
+  data() {
     return {
-      val: '',
+      val: "",
       options: [],
       props: {
-        label: 'label',
-        value: 'value'
-      }
-    }
+        label: "label",
+        value: "value",
+      },
+    };
   },
   computed: {
-    url () {
-      return this.config?.url
+    url() {
+      return this.config?.url;
     },
-    method () {
-      return this.config?.method || 'GET'
+    method() {
+      return this.config?.method || "GET";
     },
-    initRequest () {
-      return this.config?.initRequest
+    initRequest() {
+      return this.config?.initRequest;
     },
-    basePath () {
-      return this.config?.basePath
-    }
+    basePath() {
+      return this.config?.basePath;
+    },
   },
   methods: {
-    handleChangeEvent (value) {
-      this.$emit('update:value', value)
+    handleChangeEvent(value) {
+      this.$emit("update:value", value);
     },
-    initOptions () {
+    initOptions() {
       if (this.url) {
-        this.getOption()
-        return false
+        this.getOption();
+        return false;
       }
 
-      const options = this.config.options
+      const options = this.config.options;
       if (options && Array.isArray(options) && options.length > 0) {
-        this.options = options
+        this.options = options;
       }
     },
-    initProps () {
-      const props = this.config.props
-      const keys = Object.keys(this.props)
-      if (props && Object.prototype.toString.call(props) === '[object Object]') {
+    initProps() {
+      const props = this.config.props;
+      const keys = Object.keys(this.props);
+      if (
+        props &&
+        Object.prototype.toString.call(props) === "[object Object]"
+      ) {
         for (const key in props) {
           if (keys.includes(key)) {
-            this.props[key] = props[key]
+            this.props[key] = props[key];
           }
         }
       }
     },
-    async getOption () {
+    async getOption() {
       if (!this.initRequest) {
-        return false
+        return false;
       }
       const response = await this.$axios({
         url: this.url,
         method: this.method,
-        basePath: this.basePath
-      })
-      const data = response.data.data
-      this.options = data
-      console.log(this.options)
-    }
-
-  }
-}
+        basePath: this.basePath,
+      });
+      const data = response.data.data;
+      this.options = data;
+      console.log(this.options);
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
